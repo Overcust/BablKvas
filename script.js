7b2b54bdec03d5c25b5ed81a78a75d8ba6d33d53
Overcust <83780035+Overcust@users.noreply.github.com>
2021-05-16 18:08:20 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 window.onload=function(){
+    var cart = {};
+    var goods = {}
+    function showTime(){
+        var time = new Date().toLocaleTimeString()
+        document.querySelector('.date').innerHTML = time;
+    }  
+    setInterval(showTime, 1000);
     $.getJSON("https://spreadsheets.google.com/feeds/list/1LZ5bAm74UyuPibiJ0qFbxMBzEMM0gf5zk4FLmYHDF4o/od6/public/values?alt=json",
     function(data){
         data=data['feed']['entry']
         console.log(data);
+        goods = arrayHelper(data)
+        console.log(goods)
         show(data)
     }
     )
@@ -13,8 +22,39 @@ window.onload=function(){
             out +=`<h3 class="title">${data[i]['gsx$name']['$t']}</h3>`;
             out +=`<img src="${data[i]['gsx$image']['$t']}" alt="">`;
             out +=`<p class="price">Цена: ${data[i]['gsx$price']['$t']} </p>`;
+            out +=`<button name="add_to_cart" data="${data[i]['gsx$article']['$t']}">Купить</button>`;
             out +=`</div>`;
         }
         document.querySelector('.shop_field').innerHTML = out;
     }
-}
+    document.onclick = function(e){
+        console.log(e.target.attributes.name.nodeValue,e.target.attributes.data.nodeValue);
+        if(e.target.attributes.name.nodeValue == 'add_to_cart'){
+            addToCart(e.target.attributes.data.nodeValue)
+        }
+        
+
+    }
+    function addToCart(elem){
+        if(cart[elem] !== undefined){
+            cart[elem]++;
+        }
+        else{
+            cart[elem] = 1
+        }
+        console.log(cart)
+    }
+    function arrayHelper(arr){
+        var out = {};
+        for(var i = 0; i < arr.length; i++){
+            var temp = {}
+            temp['articul'] = arr[i]['gsx$article']['$t']
+            temp['name'] = arr[i]['gsx$name']['$t']
+            temp['count'] = arr[i]['gsx$count']['$t']
+            temp['price'] = arr[i]['gsx$price']['$t']
+            temp['image'] = arr[i]['gsx$image']['$t']
+            out[arr[i]['gsx$article']['$t']] = temp;
+      }   
+      return out;
+     }
+   }
